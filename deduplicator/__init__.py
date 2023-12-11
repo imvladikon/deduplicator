2e44d6f245274e10fcdd07b25da6efdd0e5dbbe2
Vladimir Gurevich <imvladikon@gmail.com>
2023-12-11 22:30:16 +0200

    init

--- deduplicator/__init__.py
+++ deduplicator/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from deduplicator.version import __version__
-from deduplicator.consolidator import AnnotationsConsolidator
 from deduplicator.deduplicator import Deduplicator
